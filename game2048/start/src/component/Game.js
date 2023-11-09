7f770b31a49c6c6bf70508316c56bae86304351c
imwisdom <kjihye0340@naver.com>
2023-11-09 15:43:27 +0900

    feature: 머지된 값에 따라 score 계산

--- game2048/start/src/component/Game.js
+++ game2048/start/src/component/Game.js
@@ -5,10 +5,10 @@ import { getInitialTileList } from '../util/tile';
 import Tile from './Tile';
 import useMoveTile from '../hook/useMoveTile';
 
-export default function Game() {
+export default function Game({setScore}) {
   const [tileList, setTileList] = useState(getInitialTileList);
   // up, down, left, right
-  useMoveTile({tileList, setTileList});
+  useMoveTile({tileList, setTileList, setScore});
   return (
     <div className="game-container">
       <div className="grid-container">
